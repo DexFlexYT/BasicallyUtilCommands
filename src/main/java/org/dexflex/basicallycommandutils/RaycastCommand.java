1eaf7d162b3dcd870077ffbde1cac6372b3936aa
DexFlex_YT <142329830+DexFlexYT@users.noreply.github.com>
2025-04-24 20:25:41 +0400

    attempted adding /raycaststop

--- src/main/java/org/dexflex/basicallycommandutils/RaycastCommand.java
+++ src/main/java/org/dexflex/basicallycommandutils/RaycastCommand.java
@@ -8,11 +8,15 @@ import net.minecraft.command.argument.EntityAnchorArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.function.CommandFunction;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec2f;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.server.world.ServerWorld;
-import java.util.Collection;
+
+import java.util.*;
+
 public class RaycastCommand {
+    private static final Set<UUID> stoppedRaycasts = Collections.synchronizedSet(new HashSet<>());
+
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(CommandManager.literal("raycast")
                 .requires(source -> source.hasPermissionLevel(4))
@@ -20,28 +24,43 @@ public class RaycastCommand {
                         .then(CommandManager.argument("step_length", FloatArgumentType.floatArg(0.01f))
                                 .then(CommandManager.argument("function", CommandFunctionArgumentType.commandFunction())
                                         .executes(ctx -> {
-                                                    Collection<CommandFunction<ServerCommandSource>> functions = CommandFunctionArgumentType.getFunctions(ctx, "function");
-                                                    int steps = IntegerArgumentType.getInteger(ctx, "steps");
-                                                    float stepLength = FloatArgumentType.getFloat(ctx, "step_length");
-                                                    ServerCommandSource source = ctx.getSource();
-                                                    ServerWorld world = source.getWorld();
-                                                    EntityAnchorArgumentType.EntityAnchor anchor = source.getEntityAnchor();
-                                                    Vec3d pos = anchor.positionAt(source);
-                                                    Vec2f rot = source.getRotation();
-                                                    Vec3d dir = Vec3d.fromPolar(rot.x, rot.y);
-                                                    for (int i = 0; i < steps; i++) {
-                                                        Vec3d stepPos = pos.add(dir.multiply(i * stepLength));
-                                                        ServerCommandSource steppedSource = source.withPosition(stepPos);
-                                                        for (CommandFunction<ServerCommandSource> function : functions) {
-                                                            world.getServer().getCommandFunctionManager().execute(function, steppedSource.withSilent());
-                                                        }
-                                                    }
-                                                    return 1;
+                                            ServerCommandSource source = ctx.getSource();
+                                            UUID sourceId = source.getEntity() != null ? source.getEntity().getUuid() : UUID.randomUUID();
+                                            stoppedRaycasts.remove(sourceId);
+
+                                            Collection<CommandFunction<ServerCommandSource>> functions = CommandFunctionArgumentType.getFunctions(ctx, "function");
+                                            int steps = IntegerArgumentType.getInteger(ctx, "steps");
+                                            float stepLength = FloatArgumentType.getFloat(ctx, "step_length");
+                                            ServerWorld world = source.getWorld();
+                                            EntityAnchorArgumentType.EntityAnchor anchor = source.getEntityAnchor();
+                                            Vec3d pos = anchor.positionAt(source);
+                                            Vec2f rot = source.getRotation();
+                                            Vec3d dir = Vec3d.fromPolar(rot.x, rot.y);
+
+                                            for (int i = 0; i < steps; i++) {
+                                                if (stoppedRaycasts.contains(sourceId)) break;
+                                                Vec3d stepPos = pos.add(dir.multiply(i * stepLength));
+                                                ServerCommandSource steppedSource = source.withPosition(stepPos);
+                                                for (CommandFunction<ServerCommandSource> function : functions) {
+                                                    world.getServer().getCommandFunctionManager().execute(function, steppedSource.withSilent());
                                                 }
-                                        )
+                                            }
+
+                                            return 1;
+                                        })
                                 )
                         )
                 )
         );
+
+        dispatcher.register(CommandManager.literal("raycaststop")
+                .requires(source -> source.hasPermissionLevel(4))
+                .executes(ctx -> {
+                    ServerCommandSource source = ctx.getSource();
+                    if (source.getEntity() != null) {
+                        stoppedRaycasts.add(source.getEntity().getUuid());
+                    }
+                    return 1;
+                }));
     }
 }
